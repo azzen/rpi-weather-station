ba11ef6b1facf1a036e0b290617ff2a3938dfbf8
Azzen <6373020+Azzen@users.noreply.github.com>
2019-12-17 15:22:01 +0100

    Add comments

--- app/middlewares/get_json_data.js
+++ app/middlewares/get_json_data.js
@@ -2,23 +2,38 @@ import fs from 'fs'
 import path from 'path'
 export default class WeatherStation {
 
+    /**
+     * Récupération des données (tous le json) et les renvoie au format json
+     */
     static getData()
     {
         let payload = fs.readFileSync(path.join(__dirname, '../static/weather.json'))
         return JSON.parse(payload)
     }
+    /**
+     * Récupération de la température
+     */
     static getTemp()
     {
         return this.getData()['temperature']
     }
+    /**
+     * Récupération de l'humidité
+     */
     static getHumidity()
     {
         return this.getData()['humidity']
     }
+    /**
+     * Récupération de la pression
+     */
     static getPressure()
     {
         return this.getData()['pressure']
     }
+    /**
+     * Récupération de la date et l'heure actuelle
+     */
     static getTime()
     {
         return this.getData()['time']
